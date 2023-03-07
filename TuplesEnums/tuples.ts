dfb1f4185b495c0f4cc7f45910303cd1ff57b758
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-07 16:20:21 -0600

    start tuple section

---
+++ TuplesEnums/tuples.ts
@@ -0,0 +1,10 @@
+//Tuples - Como un array pero mas estricto en cuanto a su capacidad
+//estan fijas en su capacidad y en su tipo
+const stuffff : string[] = ["as" , "asd", "as"]
+
+//forma correcta de crear un tuple, tengo que seguir el orden: 
+const color: [number, number, number] = [255, 0 , 255];
+
+//usar multiples tipos, aqui el orden si importa:
+
+type Response = [number, string]
