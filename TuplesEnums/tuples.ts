25952fc59ab39643eba6204e66cff8e54048a991
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-08 12:35:06 -0600

    enums section started

--- TuplesEnums/tuples.ts
+++ TuplesEnums/tuples.ts
@@ -16,3 +16,55 @@ const goodRes: HTTPResponse = [200, "OK"]
 //las tuplas no me deniegan el permiso de hacer push despues de la declaracion
 
 goodRes.push(123)
+
+//las tuplas casi no se usan tanto pero es buena saberlas
+
+
+{
+    code: 200,
+    msg: "OK"
+}
+
+//puedo hacer un array de http responses - lo de abajo es un array de tuples
+const responses: HTTPResponse[] = [[404, "not found"], [200, "OK"]]
+
+
+
+//ENUMS
+
+//me permiten definir un set de constantes con nombre
+//a las constantes les podemos pasar un valor numero o strings
+
+//estos son los 4 estados que acepto, me ahorra tener varias variables
+//puedo usar mi set por todo mi codigo y asi re utilizar
+// enum OrderStatus {
+//     PENDING,
+//     SHIPPED,
+//     DELIVERED,
+//     RETURNED
+// }
+
+// const myStatus = OrderStatus.DELIVERED;
+
+// function isDelivered(status: OrderStatus) {
+//     return status === OrderStatus.DELIVERED
+// }
+
+// isDelivered(OrderStatus.RETURNED)
+
+//puedo asignar un valor a mis constantes
+enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED
+}
+
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+    return status === OrderStatus.DELIVERED
+}
+
+isDelivered(OrderStatus.RETURNED)
+
