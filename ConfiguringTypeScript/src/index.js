4c8cd9c52edb00f8ffd95ed0b477b3634e2edea9
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-04 17:02:43 -0600

    include - exclude added to tsconfig.json

---
+++ ConfiguringTypeScript/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: 'Silkie',
+    eggsPerWeek: 4,
+    name: 'Norma',
+    age: 2
+};
+const juniper = {
+    breed: 'Silkie',
+    eggsPerWeek: 8,
+    name: 'juniper',
+    age: 1
+};
